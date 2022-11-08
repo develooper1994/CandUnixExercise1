cdcc9503994b9c4b1b6d9349d644c93411242f47
Mustafa Selçuk Çağlar <11412546+develooper1994@users.noreply.github.com>
2022-11-08 21:54:11 +0300

    Update doublevector.c

--- doublevector.c
+++ doublevector.c
@@ -1,4 +1,4 @@
-// ----------------------------- <doubleVector> -----------------------------------
+// ----------------------------- <doublevector> -----------------------------------
 #include "doublevector.h"
 
 void initialize(DoubleVector* dv, size_t size){
@@ -9,13 +9,15 @@ void initialize(DoubleVector* dv, size_t size){
 
 void deallocate(DoubleVector* dv){
     free(dv->data);
-    dv->data = NULL;
+    dv->data = NULL; // TODO: ???
     dv->size = 0;
     dv->__capacity = 0;
 }
 
-void resize(DoubleVector* dv, int new_size){
+void resize(DoubleVector* dv, size_t new_size){
+    // https://cplusplus.com/forum/general/263662/
     if (new_size + 1 > dv->__capacity){
+        // if data is not empty then resize otherwise allocate for it.
         char* new_data = dv->data ? 
             (char*)realloc( dv->data, new_size + 1 )
             : (char*)malloc( new_size + 1 );
@@ -29,23 +31,21 @@ void resize(DoubleVector* dv, int new_size){
         dv->size = new_size;
 }
 
-void append(char* buffer, size_t buffer_size){
-    char* buffer_new = (char*)realloc(buffer, sizeof(char)*buffer_size);
-    for (size_t index = 0; index < buffer_size; ++index) {
-        buffer_new[index] = buffer[index];
-        //buffer[index] = NULL;
-    }
-    buffer = buffer_new;
+void append(DoubleVector* dv, char element){
+    size_t buffer_size = dv->__capacity * CAPACITY_FACTOR + 1;
+    size_t element_index = buffer_size - 1;
+    resize(dv, buffer_size);
+    dv->data[element_index] = element;
 }
 
-void appendPointer(char** buffer, size_t buffer_size){
-    char** buffer_new = (char**)realloc(buffer, sizeof(char)*buffer_size);
-    for (size_t index = 0; index < buffer_size; ++index) {
-        buffer_new[index] = buffer[index];
-        //buffer[index] = NULL;
-    }
-    buffer = buffer_new;
+void appendPointer(DoubleVector** dv_pointer, DoubleVector* dv){
+    DoubleVector* dv_temp = *dv_pointer; // TODO: ???
+    size_t buffer_size = dv_temp->__capacity * CAPACITY_FACTOR + 1;
+    size_t element_index = buffer_size - 1;
+    resize(*dv_pointer, buffer_size);
+    dv->data[element_index] = element;
+    
 }
 
 
-// ----------------------------- </doubleVector> -----------------------------------
+// ----------------------------- </doublevector> -----------------------------------
