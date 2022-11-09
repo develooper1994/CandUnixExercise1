b023e4303c10f5824dc3e0701f60d686c0247de2
Mustafa Selçuk Çağlar <11412546+develooper1994@users.noreply.github.com>
2022-11-09 21:24:52 +0300

    Update doublevector.c

--- doublevector.c
+++ doublevector.c
@@ -1,10 +1,19 @@
 // ----------------------------- <doublevector> -----------------------------------
 #include "doublevector.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 DoubleVector* CreateDoubleVector(size_t size){
     DoubleVector* initialization = (DoubleVector*)malloc(sizeof(DoubleVector)*1);
-    InitializeDoubleVector(initialization, size);
+    if (!initialization) {
+        return NULL;
+    }
+    int ret = InitializeDoubleVector(initialization, size);
+    if(ret == EXIT_FAILURE){
+        free(initialization);
+        return NULL;
+    }
+
     return initialization;
 }
 
@@ -14,10 +23,14 @@ void DestroyDoubleVector(DoubleVector* dv){
     dv = NULL;
 }
 
-void InitializeDoubleVector(DoubleVector* dv, size_t size){
+int InitializeDoubleVector(DoubleVector* dv, size_t size){
     dv->data = (int*)malloc(sizeof(int)*size);
-    dv->size = size;
+    if (!dv->data) 
+        return EXIT_FAILURE;
+    dv->size = 0;
     dv->__capacity = size;
+
+    return EXIT_SUCCESS;
 }
 
 void DeallocateDoubleVector(DoubleVector* dv){
@@ -27,28 +40,43 @@ void DeallocateDoubleVector(DoubleVector* dv){
     dv->__capacity = 0;
 }
 
-void ResizeDoubleVector(DoubleVector* dv, size_t new_size){
+int ResizeDoubleVector(DoubleVector* dv, size_t new_size){
     // https://cplusplus.com/forum/general/263662/
     if (new_size + 1 > dv->__capacity){
         // if data is not empty then resize otherwise allocate for it.
         int* new_data = dv->data ? 
-            (int*)realloc( dv->data, new_size + 1 )
-            : (int*)malloc( new_size + 1 );
+            (int*)realloc(dv->data, new_size + 1) :
+            (int*)malloc(sizeof(int)*(new_size + 1));
         if (new_data){
             dv->__capacity = new_size + 1;
             dv->size     = new_size;
             dv->data     = new_data;
         }
+        else
+            return EXIT_FAILURE;
     }
     else
         dv->size = new_size;
+
+    return EXIT_SUCCESS;
 }
 
-void AppendDoubleVector(DoubleVector* dv, int element){
-    size_t buffer_size = dv->__capacity * CAPACITY_FACTOR + 1;
-    size_t element_index = buffer_size - 1;
-    ResizeDoubleVector(dv, buffer_size);
+int AppendDoubleVector(DoubleVector* dv, int element){
+    // TODO: doesn't enter t the function due to "DoubleVector* dv"
+    puts("<buffer_size>"); // debug
+    size_t new_capacity = dv->__capacity * CAPACITY_FACTOR + 1;
+    puts("<element_index>"); // debug
+    size_t element_index = new_capacity - 1;
+    puts("<ResizeDoubleVector>"); // debug
+    int ret = ResizeDoubleVector(dv, new_capacity);
+
+    if(ret == EXIT_FAILURE) return EXIT_FAILURE;
+
+    puts("</ResizeDoubleVector>"); // debug
     dv->data[element_index] = element;
+    puts("</dv->data[element_index] = element;>"); // debug
+
+    return EXIT_SUCCESS;
 }
 
 size_t AtDoubleVector(DoubleVector* dv, size_t index){
